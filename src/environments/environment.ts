ec86ce0a4b5fe78b8b2cab683941bd6e3952cd6e
Witer Mendonça <mendoncawiter@gmail.com>
2024-11-03 11:01:58 -0300

    modificando url da api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    // url: 'http://localhost:8080/',
-    url: 'https://fatec-aba-project-backend.onrender.com/',
+    url: 'http://localhost:8080/',
+    // url: 'https://fatec-aba-project-backend.onrender.com/',
 };
